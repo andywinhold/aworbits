79826edfa7bf9622d58e617e12ddb0ed3f644a60
Jordan Boyd <jbboyd@asu.edu>
2016-04-09 10:11:56 -0700

    cleaning up work director

--- Work/rama.py
+++ Work/rama.py
@@ -1,6 +1,43 @@
 import numpy as np
 
+
+#============================================================
+# Parameters of the problem
+#------------------------------------------------------------
+#
+
 G_gravity = 4*np.pi**2
+
+# mass in AU
+mass = {'Sun': 1.,
+        'Uranus': 4.366244e-5,
+        'Neptune': 5.151389e-5,
+}
+
+# orbital period in Earth years
+period = {'Uranus': 84.0110,
+          'Neptune': 164.7901,
+}
+
+def initial_position(angle, distance):
+    """Calculate initial planet position.
+    Parameters
+    ----------
+    angle : float
+       initial angle relative to x axis (in degrees)
+    distance : float
+       initial distance from sun (in AU)
+       
+       
+       
+    Returns
+    -------
+    array
+       position (x, y)
+    """
+    x = np.deg2rad(angle)
+    return distance * np.array([np.cos(x), np.sin(x)])
+
 
 def gravity(coordinates, masses):
     """Calculates total force on each solar system object using Newtonian gravity.
@@ -16,7 +53,7 @@ def gravity(coordinates, masses):
     Returns
     -------
     force : array
-        Returns net force on each solar system object as '(N, 3)' array and total potential as scalar.
+        Returns net force on each solar system object as (x,y,z) array and total potential as scalar.
     """
     
     N = len(coordinates) #number of objects
