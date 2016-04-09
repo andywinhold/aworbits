eb955d0689901f70629d77ccf1cf44379880a7d6
Jordan Boyd <jbboyd@asu.edu>
2016-04-09 10:38:11 -0700

    initial working dynamics model

--- Work/rama.py
+++ Work/rama.py
@@ -19,26 +19,6 @@ period = {'Uranus': 84.0110,
           'Neptune': 164.7901,
 }
 
-def initial_position(angle, distance):
-    """Calculate initial planet position.
-    Parameters
-    ----------
-    angle : float
-       initial angle relative to x axis (in degrees)
-    distance : float
-       initial distance from sun (in AU)
-       
-       
-       
-    Returns
-    -------
-    array
-       position (x, y)
-    """
-    x = np.deg2rad(angle)
-    return distance * np.array([np.cos(x), np.sin(x)])
-
-
 def gravity(coordinates, masses):
     """Calculates total force on each solar system object using Newtonian gravity.
     
@@ -72,6 +52,51 @@ def gravity(coordinates, masses):
             dF =  rhat * G_gravity * masses[i] * masses[j] / (rmag**2)
             force[j] += dF
             force[i] += -dF
-            U += -G_gravity * masses[i] * masses[j] / rmag    
+            U += G_gravity * masses[i] * masses[j] / rmag    
             
     return force, U
+
+def dynamics(x0, v0, dt, masses, nsteps=10):
+    """Integrate equations of motions
+    Parameters
+    ----------
+    x0 : array
+         Nx3 array containing the starting coordinates of the planetary objects.
+         (Note that x0 is changed and at the end of the run will
+         contain the last coordinates.)
+    v0 : array
+         Nx3 array containing the starting velocities, eg np.zeros((N,3))
+     
+    masses : array
+        masses for every object in solar system
+
+    dt : float
+         integration timestep in units?
+         
+    nsteps : int, optional
+         number of integrator time steps
+         
+    Returns array of coordinates of planetary objects, their velocities, total kinetic, potential, and overall energies.
+    """
+    
+    N = len(x0) #number of objects
+    x = np.copy(x0)
+    v = np.copy(v0)
+    vhalf = np.zeros((N,3))
+    Ut = np.zeros(nsteps)
+    kinetic = np.zeros(nsteps)
+    totalE = np.zeros(nsteps)
+    
+    Ft, Ut[0]= gravity(x, masses)
+    
+    for i in range(nsteps):
+        for j in range(N):
+            vhalf[j] = v[j] + 0.5 * dt * Ft[j] / masses[j]
+            x[j] += dt * vhalf[j]
+        Ft, Ut[i]= gravity(x, masses)
+        for j in range(N):
+            v[j] = vhalf[j] + 0.5 * dt * Ft[j] / masses[j]
+            kinetic[i] += 0.5 * masses[j] * np.sum(v[j]**2) 
+        totalE[i] = kinetic[i] + Ut[i] 
+            
+    return x, v, kinetic, Ut, totalE
