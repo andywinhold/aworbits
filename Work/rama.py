55f65cdb7594db9709cbc65f3b25ddb0758a8328
Andy <andrew.winhold@asu.edu>
2016-04-16 01:52:18 -0700

    edits to rama.py and created citations.txt

--- Work/rama.py
+++ Work/rama.py
@@ -15,10 +15,11 @@ mass = np.array([
             4.86732/(1.989e6),  #Venus
             5.97219/(1.989e6),  #Earth
             0.641693/(1.989e6), #Mars
-            1898.13/(1.989e6),  #Jupiter
+            1898.19/(1.989e6),  #Jupiter
             568.319/(1.989e6),  #Saturn
             86.8103/(1.989e6),  #Uranus
             102.410/(1.989e6),  #Neptune
+            0.0166/(1.989e6),   #Eris
             ])
 
 #Semi-Major Axis in AU
@@ -28,10 +29,11 @@ smAxis = np.array([
             0.72333566,  #Venus
             1.00000261,  #Earth
             1.52371034,  #Mars
-            5.20288700,  #Jupiter
+            5.200441899,  #Jupiter
             9.53667594,  #Saturn
             19.18916464, #Uranus
-            30.06992276  #Neptune
+            30.06992276, #Neptune
+            67.68871444,# Eris
             ])
 
 # Eccentricity
@@ -44,7 +46,8 @@ eccentricity = np.array([
             0.04838624,  #Jupiter
             0.05386179,  #Saturn
             0.04725744,  #Uranus
-            0.00859048   #Neptune
+            0.00859048,  #Neptune
+            0.44068      #Eris
             ])
 
 #Inclination in degrees
@@ -57,7 +60,8 @@ inclination = np.array([
             1.30439695,  #Jupiter
             2.48599187,  #Saturn
             0.77263783,  #Uranus
-            1.77004347   #Neptune
+            1.77004347,   #Neptune
+            44.0445      #Eris
             ])
 inclination = np.deg2rad(inclination) #converting to radians
 
@@ -71,7 +75,8 @@ mLong = np.array([
             34.39644051,   #Jupiter
             49.95424423,   #Saturn
             313.23810451,  #Uranus
-            -55.12002969   #Neptune
+            -55.12002969,  #Neptune
+            204.16         #Eris
             ])
 mLong = np.deg2rad(mLong) #converting to radians
 
@@ -85,7 +90,9 @@ pLong = np.array([
             14.72847983,   #Jupiter
             92.59887831,   #Saturn
             170.95427630,  #Uranus
-            44.96476227    #Neptune
+            44.96476227,   #Neptune
+            187.1498689    #Eris ** Found by adding argument of perihelion
+                           #and longitude of the ascending node, per JPL details.
             ])
 pLong = np.deg2rad(pLong) #converting to radians
 
@@ -93,19 +100,48 @@ pLong = np.deg2rad(pLong) #converting to radians
 aLong = np.array([
             0.,            #Sun
             48.33076593,   #Mercury
-            76.67984255,  #Venus
-            0.0,  #Earth
-            49.55953891,  #Mars
-            100.47390909,   #Jupiter
-            113.66242448,   #Saturn
-            74.01692503,  #Uranus
-            131.78422574    #Neptune
+            76.67984255,   #Venus
+            0.0,           #Earth
+            49.55953891,   #Mars
+            100.47390909,  #Jupiter
+            113.66242448,  #Saturn
+            74.01692503,   #Uranus
+            35.906450258,  #Neptune
+            131.78422574   #Eris
             ])
 aLong = np.deg2rad(aLong) #converting to radians
 
+# Additional terms for Mean Anomaly of Jupiter - Neptune, Eris TBD
+b = np.array([
+        -0.00012452,       #Jupiter
+        0.00025899,        #Saturn
+        0.00058331,        #Uranus
+        -0.00041348        #Neptune
+        ])
+
+c = np.array([
+        0.06064060,        #Jupiter
+        -0.13434469,       #Saturn
+        -0.97731848,       #Uranus
+        0.68346318         #Neptune
+        ])
+
+s = np.array([
+        -0.35635438,       #Jupiter
+        0.87320147,        #Saturn
+        0.17689245,        #Uranus
+        -0.10162547        #Neptune
+        ])
+
+f = np.array([
+        38.35125000,       #Jupiter
+        38.35125000,       #Saturn
+        7.67025000,        #Uranus
+        7.67025000         #Neptune
+        ])
+        
 #Dictionary of solar system properties
-system = {"Mass":mass, "Semi-Major Axis":smAxis, "Eccentricity":eccentricity, "Inclination":inclination, "Mean Longitude":mLong,
-          "Perihelion Longitude":pLong, "Ascending Longitude":aLong}
+system = {"Mass":mass, "Semi-Major Axis":smAxis, "Eccentricity":eccentricity, "Inclination":inclination, "Mean Longitude":mLong,"Perihelion Longitude":pLong, "Ascending Longitude":aLong, "b":b, "c":c, "s":s, "f":f}
 
 def getCoordinates():
     """Calculates cartesian coordinates of Heliocentric solar system for each solar system object.
@@ -115,11 +151,32 @@ def getCoordinates():
     coordinates : array
         Returns coordinates of every object as (x,y,z) array, with the sun at the center.
     """
+    #Find number of centuries past J2000, when our scenario starts I think
+    
+    #additional equations (especially 5.48) from ~page 214 of:
+    #http://disciplinas.stoa.usp.br/pluginfile.php/66104/mod_resource/conten\/1/OrbitalMechanicsForEngineeringStudents-AerospaceEngineering.pdf
+    
+    #year, month, day of interest of interest
+    y = 2016
+    m = 4
+    d = 29 
+    #Julian day
+    J = 367*y - np.int((7/4)*(y+np.int((m+9/12)))) + np.int((275*m)/9) + d + 1721013.5
+    #Ephemeris time, eqn. 5.49 in above textbook, also in point 1. in JPL doc
+    # 2451545 is the exact julian day number for J2000
+    # 36525 is the number of days a julian century has
+    T = (J - 2451545)/36525
+                        
     tol = 1e-5
     coordinates = np.zeros((len(system["Mass"]), 3))
     
     omega = np.degrees(system["Perihelion Longitude"]) - np.degrees(system["Ascending Longitude"])
+    #mean anom for merc - mars
     meanAnomaly = (system["Mean Longitude"] - system["Perihelion Longitude"]) % np.pi
+    #mean anom for jup - nept, eris tbd
+#     for i in range(system["Semi-Major Axis"][4:8]):
+#         meanAnomaly = (system["Mean Longitude"][i] - system["Perihelion Longitude"][i]) + system["b"][i]*T**2 + system["c"][i]*np.cos(system["f"][i]*T) + system["s"][i]*np.sin(system["f"]*T) 
+                        
     E = meanAnomaly + np.deg2rad(system["Eccentricity"]) * np.sin(meanAnomaly)
     deltaE = np.zeros_like(E)
                            
