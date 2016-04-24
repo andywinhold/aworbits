28f4148efd6d573740381eaf7797262b63d00abf
Jordan Boyd <jbboyd@asu.edu>
2016-04-24 12:21:34 -0700

    working solar system

--- Work/rama.py
+++ Work/rama.py
@@ -6,7 +6,7 @@ import numpy as np
 #------------------------------------------------------------
 #
 
-G_gravity = 4*np.pi**2
+G_gravity = 3.964016 * 9.945193
 
 #Masses in solar masses
 mass = np.array([
@@ -63,7 +63,7 @@ inclination = np.array([
             1.77004347,   #Neptune
             44.0445      #Eris
             ])
-inclination = np.deg2rad(inclination) #converting to radians
+#inclination = np.deg2rad(inclination) #converting to radians
 
 #Mean Longitude in degrees
 mLong = np.array([
@@ -78,7 +78,7 @@ mLong = np.array([
             -55.12002969,  #Neptune
             204.16         #Eris
             ])
-mLong = np.deg2rad(mLong) #converting to radians
+#mLong = np.deg2rad(mLong) #converting to radians
 
 #Longitude of perihelion in degrees
 pLong = np.array([
@@ -94,7 +94,7 @@ pLong = np.array([
             187.1498689    #Eris ** Found by adding argument of perihelion
                            #and longitude of the ascending node, per JPL details.
             ])
-pLong = np.deg2rad(pLong) #converting to radians
+#pLong = np.deg2rad(pLong) #converting to radians
 
 #Longitude of ascending node in degrees
 aLong = np.array([
@@ -109,7 +109,7 @@ aLong = np.array([
             35.906450258,  #Neptune
             131.78422574   #Eris
             ])
-aLong = np.deg2rad(aLong) #converting to radians
+#aLong = np.deg2rad(aLong) #converting to radians
 
 # Additional terms for Mean Anomaly of Jupiter - Neptune, Eris TBD
 b = np.array([
@@ -143,68 +143,7 @@ f = np.array([
 #Dictionary of solar system properties
 system = {"Mass":mass, "Semi-Major Axis":smAxis, "Eccentricity":eccentricity, "Inclination":inclination, "Mean Longitude":mLong,"Perihelion Longitude":pLong, "Ascending Longitude":aLong, "b":b, "c":c, "s":s, "f":f}
 
-def getCoordinates():
-    """Calculates cartesian coordinates of Heliocentric solar system for each solar system object.
-
-    Returns
-    -------
-    coordinates : array
-        Returns coordinates of every object as (x,y,z) array, with the sun at the center.
-    """
-    #Find number of centuries past J2000, when our scenario starts I think
-    
-    #additional equations (especially 5.48) from ~page 214 of:
-    #http://disciplinas.stoa.usp.br/pluginfile.php/66104/mod_resource/conten\/1/OrbitalMechanicsForEngineeringStudents-AerospaceEngineering.pdf
-    
-    #year, month, day of interest of interest
-    y = 2016
-    m = 4
-    d = 29 
-    #Julian day
-    J = 367*y - np.int((7/4)*(y+np.int((m+9/12)))) + np.int((275*m)/9) + d + 1721013.5
-    #Ephemeris time, eqn. 5.49 in above textbook, also in point 1. in JPL doc
-    # 2451545 is the exact julian day number for J2000
-    # 36525 is the number of days a julian century has
-    T = (J - 2451545)/36525
-                        
-    tol = 1e-5
-    coordinates = np.zeros((len(system["Mass"]), 3))
-    
-    omega = np.degrees(system["Perihelion Longitude"]) - np.degrees(system["Ascending Longitude"])
-    #mean anom for merc - mars
-    meanAnomaly = (system["Mean Longitude"] - system["Perihelion Longitude"]) % np.pi
-    #mean anom for jup - nept, eris tbd
-#     for i in range(system["Semi-Major Axis"][4:8]):
-#         meanAnomaly = (system["Mean Longitude"][i] - system["Perihelion Longitude"][i]) + system["b"][i]*T**2 + system["c"][i]*np.cos(system["f"][i]*T) + system["s"][i]*np.sin(system["f"]*T) 
-                        
-    E = meanAnomaly + np.deg2rad(system["Eccentricity"]) * np.sin(meanAnomaly)
-    deltaE = np.zeros_like(E)
-                           
-    count = 0
-    while np.abs(np.linalg.norm(deltaE)) > tol:
-        deltaE = (M - (E - np.deg2rad(system["Eccentricity"]) * np.sin(E))) / (1 - system["Eccentricity"] * np.cos(E))
-        E = E + deltaE
-        count += 1
-        if count > 1000:
-            print("E did not converge to a solution after 1000 iterations.")
-            break
-    
-    #calculates heliocentric coordinates in orbital plane
-    coordinates[:, 0] = system["Semi-Major Axis"] * (np.cos(E) - system["Eccentricity"])
-    coordinates[:, 1] = system["Semi-Major Axis"] * np.sin(E) * np.sqrt(1 - system["Eccentricity"]**2)
-                  
-    #calculates heliocentric coordinates in ecliptic plane
-    coordinates[:, 0] = (np.cos(omega) * np.cos(system["Ascending Longitude"]) - 
-                         np.sin(omega) * np.sin(system["Ascending Longitude"]) * 
-                         np.cos(system["Inclination"])) * coordinates[:, 0] + (np.sin(omega) * np.cos(system["Ascending Longitude"]) - np.cos(omega) * np.sin(system["Ascending Longitude"]) * np.cos(system["Inclination"])) * coordinates[:, 1]
-    coordinates[:, 1] = (np.cos(omega) * np.sin(system["Ascending Longitude"]) + 
-                         np.sin(omega) * np.cos(system["Ascending Longitude"]) * 
-                         np.cos(system["Inclination"])) * coordinates[:, 0] + (-np.sin(omega) * np.sin(system["Ascending Longitude"]) + np.cos(omega) * np.cos(system["Ascending Longitude"]) * np.cos(system["Inclination"])) * coordinates[:, 1]
-    coordinates[:, 2] = (np.sin(omega) * np.sin(system["Inclination"])
-                        ) * coordinates[:, 0] + (np.cos(omega) * np.sin(system["Inclination"])) * coordinates[:, 1]
-    
-    return coordinates
-    
+period = np.array([0, 87.969 / 365.256, 224.701 / 365.256, 365.256 / 365.256, 1.8808, 11.8618, 29.4571, 84.0110, 164.7901, 558.04])
 
 def gravity(coordinates, masses):
     """Calculates total force on each solar system object using Newtonian gravity.
@@ -228,6 +167,8 @@ def gravity(coordinates, masses):
     force = np.zeros((N, 3)) #force between objects
     U = 0 #potential energy
     
+    masses = np.array(system["Mass"])
+    
     for i in range(0, N - 1):
         for j in range(i + 1, N):
             #calculates distance vector between each object
@@ -236,14 +177,14 @@ def gravity(coordinates, masses):
             #force calculation
             rmag = np.sqrt(r[i, 0]**2 + r[i, 1]**2 + r[i, 2]**2)
             rhat = r[i] / rmag
-            dF =  -rhat * G_gravity * masses[i] * masses[j] / (rmag**2)
-            force[j] += dF
-            force[i] += -dF
+            dF =  rhat * G_gravity * masses[i] * masses[j] / (rmag**2)
+            force[j] += -dF
+            force[i] += dF
             U += -G_gravity * masses[i] * masses[j] / rmag    
             
     return force, U
 
-def dynamics(x0, v0, dt, masses, tmax=10):
+def dynamics(x0, v0, dt, tmax=10):
     """Integrate equations of motions
     Parameters
     ----------
@@ -275,6 +216,8 @@ def dynamics(x0, v0, dt, masses, tmax=10):
     Ut = np.zeros(nsteps)
     kinetic = np.zeros(nsteps)
     totalE = np.zeros(nsteps)
+    
+    masses = np.copy(system["Mass"])
     
     Ft, Ut[0]= gravity(dx, masses)
     
